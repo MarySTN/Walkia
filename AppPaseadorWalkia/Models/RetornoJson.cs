889b4bb2074d5b2b65d285a39eac2c699098b49f
MarySTN <85576791+MarySTN@users.noreply.github.com>
2021-08-07 00:24:11 -0500

    AppWalkia.android

---
+++ AppPaseadorWalkia/Models/RetornoJson.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdminAppTurismo.Models
+{
+    public class RetornoJson
+    {
+
+        [JsonProperty("conteo")]
+        public int existe = 0;
+        [JsonProperty("nombre")]
+        public string nombre = "";
+        [JsonProperty("apellido")]
+        public string apellido = "";
+        [JsonProperty("Correo")]
+        public string Correo = "";
+        [JsonProperty("Gen")]
+        public string Gen = "";
+        [JsonProperty("Fecha")]
+        public string Fecha = "";
+        [JsonProperty("id")]
+        public string id = "";
+        [JsonProperty("ruta")]
+        public string ruta = "";
+        [JsonProperty("ruta")]
+        public string foto = "";
+        [JsonProperty("pass")]
+        public string pass = "";
+
+    }
+    public class RetornoCheckEmail
+    {
+        [JsonProperty("conteo")]
+        public int existe = 0;
+    }
+}
